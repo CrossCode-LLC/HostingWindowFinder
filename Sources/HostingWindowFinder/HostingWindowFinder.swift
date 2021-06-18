745c20863f01d859d263f2074c9688d95bc1a294
Alexander Cross <crosscode.llc@gmail.com>
2021-06-17 21:20:09 -0500

    first commit

--- Sources/HostingWindowFinder/HostingWindowFinder.swift
+++ Sources/HostingWindowFinder/HostingWindowFinder.swift
@@ -1,3 +1,17 @@
-struct HostingWindowFinder {
-    var text = "Hello, World!"
+import Foundation
+import SwiftUI
+
+@available(iOS 13, *)
+struct HostingWindowFinder: UIViewRepresentable {
+    var callback: (UIWindow?) -> ()
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async { [weak view] in
+            self.callback(view?.window)
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
 }
